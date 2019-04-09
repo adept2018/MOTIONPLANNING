d161d72c3a9799327d9d625e3d96e47e4e73a8e6
EN7R0P1 <chchamal@gmail.com>
2019-04-09 19:51:28 +0200

    added a vec3 class just in case

--- advanced_motion_planner/include/advanced_motion_planner/util.h
+++ advanced_motion_planner/include/advanced_motion_planner/util.h
@@ -6,38 +6,35 @@ namespace math {
 // thus private members
   class vec2 {
   public:
-    constexpr vec2() = delete;
-    constexpr vec2(const vec2&) = default;
-    constexpr vec2(vec2&&) = default;
+      constexpr vec2() = delete;
+      constexpr vec2(const vec2&) = default;
+      constexpr vec2(vec2&&) = default;
 
-    constexpr explicit vec2(float point): x(point), y(point) {}
-    constexpr explicit vec2(float ox, float oy): x(ox), y(oy) {}
+      constexpr explicit vec2(float point): x(point), y(point) {}
+      constexpr explicit vec2(float ox, float oy): x(ox), y(oy) {}
 
-    // constexpr vec2(const vec2& point): mX(point.GetX()), mY(point.GetY()) {}
+      // constexpr vec2(const vec2& point): mX(point.GetX()), mY(point.GetY()) {}
 
-    // constexpr vec2& operator=(const vec2&) = default;
-    // constexpr vec2& operator=(vec2&&) = default;
+      // constexpr vec2& operator=(const vec2&) = default;
+      // constexpr vec2& operator=(vec2&&) = default;
 
   public:
-    float x;
-    float y;
+      float x;
+      float y;
   };
 
   class vec3 {
   public:
-    vec3() = delete;
-    vec3(float x, float y, float z): mX(x), mY(y), mZ(z) {}
-    vec3(float point): mX(point), mY(point), mZ(point) {}
-    vec3(const vec3& point): mX(point.GetX()), mY(point.GetY()), mZ(point.GetZ()){};
+      constexpr vec3() = delete;
+      constexpr vec3(const vec3&) = default;
+      constexpr vec3(vec3&&) = default;
 
-    inline float GetX() const { return mX;}
-    inline float GetY() const { return mY;}
-    inline float GetZ() const { return mZ;}
-
-  private:
-    float mX;
-    float mY;
-    float mZ;
+      constexpr explicit vec3(float point): x(point), y(point), z(point) {}
+      constexpr explicit vec3(float ox, float oy, float oz): x(ox), y(oy), z(oz) {}
+  public:
+      float x;
+      float y;
+      float z;
   };
 
 };
