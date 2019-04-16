1db84495d7545dab81626193142c8e1321ea4e6f
EN7R0P1 <chchamal@gmail.com>
2019-04-16 17:48:19 +0200

    added list of allowed ranges

--- advanced_motion_planner/include/advanced_motion_planner/MotionComputer.h
+++ advanced_motion_planner/include/advanced_motion_planner/MotionComputer.h
@@ -7,6 +7,16 @@
 #include <sensor_msgs/LaserScan.h>
 #include <advanced_motion_planner/LidarToCloudConverter.h>
 
+struct Range {
+    float AngleA = 0.0f;
+    float DistanceA = 0.0f;
+    float AngleB = 0.0f;
+    float DistanceB = 0.0f;
+
+    constexpr Range(float angleA, float distanceA, float angleB, float distanceB):
+        AngleA(angleA), DistanceA(distanceA), AngleB(angleB), DistanceB(distanceB){}
+}
+
 class MotionComputer {
 
 public:
@@ -24,6 +34,8 @@ private:
     void scanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan);
 
 private:
+    pcl::PointCloud<pcl::PointXYZ> mCloud;         // allocate a buffer for that?
+    std::vector<Range> mRanges;
     pcl::PointCloud<pcl::PointXYZ> mCloud;         // allocate a buffer for that?
     std::queue<sensor_msgs::LaserScan> mScanQueue; //
     LidarToCloudConverter mLidarToCloudConverter;  //
